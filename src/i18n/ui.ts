09a660c5ae7d27c0398f4570eb6afee36053a07f
pablopuch <pablopuch21@hotmail.com>
2024-06-27 13:21:49 +0100

    add translate web page

---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const ui = {
+  es: {
+    "site.title": "Libella",
+    "sub-header": "Centro de vida y bienestar",
+    "site.description": "Libella centro de vida y bienestar",
+    "nav.home": "Inicio",
+    "nav.about": "Libella",
+    "nav.class": "Clases",
+    "nav.contact": "Contacto",
+  },
+  en: {
+    "site.title": "Libella",
+    "sub-header": "Life and Wellness Centre",
+    "site.description": "Libella centro de vida y bienestar",
+    "nav.home": "Home",
+    "nav.about": "Libella",
+    "nav.class": "Class",
+    "nav.contact": "Contact",
+  },
+} as const;
