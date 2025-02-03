59e8c035324c22c64b51ca2f480dd2ea880e520c
pablopuch <pablopuch21@hotmail.com>
2025-02-03 21:43:46 +0000

    first commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const ui = {
+  en: {
+    "home.welcome": "Welcome to my website",
+    "site.title": "Libella",
+    "sub-header": "Life and Wellness Centre",
+    "site.description": "Libella centro de vida y bienestar",
+    "nav.home": "Home",
+    "nav.class": "Class",
+    "nav.contact": "Contact",
+
+  },
+  es: {
+    "home.welcome": "Bienvenido a mi sitio web",
+    "site.title": "Libella",
+    "sub-header": "Centro de vida y bienestar",
+    "site.description": "Libella centro de vida y bienestar",
+    "nav.home": "Inicio",
+    "nav.class": "Clases",
+    "nav.contact": "Contacto",
+  },
+} as const;
